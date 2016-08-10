8bc7e3c9a58ca3a7fe60b96136b9c5ed688d0c07
guqq <guqq@pku.edu.cn>
2016-08-10 23:09:15 +0800

    Add mkl_complex learning

---
+++ mkl_complex/README.md
@@ -0,0 +1,4 @@
+# Deal with Complex Matrix with MKL
+In order to deal with Hermitian Matrix eigen-problem, This folder contains learning process of how to deal with complex matrix with mkl library.
+
+very basic!
---
+++ mkl_complex/example.c
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <mkl_cblas.h>
+
+#define N	10
+void initVector(MKL_Complex16 * v)
+{
+	for (int i = 0; i < N; i++) {
+		v[i].real = -i * 1.0 f;
+		v[i].imag = i * 1.0 f;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	MKL_Complex16  *vector = (MKL_Complex16 *) malloc(sizeof(MKL_Complex16) * N);
+	initVector(vector);
+
+	double	ret3 = cblas_dzasum(N, vector, 1);
+	//复数
+		printf("Result of sasum: %lf\n", ret3);
+}
