6f84416c203708c1cc90d183024fc126630a31d0
guqq <guqq@pku.edu.cn>
2016-08-11 15:07:45 +0800

    tune example.c to be suitable

--- mkl_complex/example.c
+++ mkl_complex/example.c
@@ -1,14 +1,14 @@
-#include "stdafx.h"
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <mkl_cblas.h>
 
 #define N	10
 void initVector(MKL_Complex16 * v)
 {
-	for (int i = 0; i < N; i++) {
-		v[i].real = -i * 1.0 f;
-		v[i].imag = i * 1.0 f;
+	int i;
+	for (i = 0; i < N; i++) {
+		v[i].real = -i * 1.0f;
+		v[i].imag = i * 1.0f;
 	}
 }
 
@@ -17,7 +17,6 @@ int	main(int argc, char *argv[])
 	MKL_Complex16  *vector = (MKL_Complex16 *) malloc(sizeof(MKL_Complex16) * N);
 	initVector(vector);
 
-	double	ret3 = cblas_dzasum(N, vector, 1);
-	//复数
+	double	ret3 = cblas_dzasum(N, vector, 1); //cal magnititude. return a double number.
 		printf("Result of sasum: %lf\n", ret3);
 }
